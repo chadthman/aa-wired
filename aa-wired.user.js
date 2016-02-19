6bb3159d95c39e61dec833748d1d5df9c97ccdec
akadrac <github@drac.id.au>
2016-02-19 14:11:08 +1100

    firefox had a different string

--- aa-wired.user.js
+++ aa-wired.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         aa-wired
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  replace javascript load of ad blocker
 // @author       akadrac
 // @match        http://*.wired.com/*
@@ -17,11 +17,17 @@
 var adScript = document.evaluate("//script", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null); 
 
 var bad  = "http://www.wired.com/assets/load?scripts=true&c=1&load%5B%5D=jquery-sonar,wpcom-lazy-load-images,outbrain,tracking,ads,wired&ver=1455828505";
+var ffBad = "http://www.wired.com/assets/load?scripts=true&amp;c=1&amp;load%5B%5D=jquery-sonar,wpcom-lazy-load-images,outbrain,tracking,ads,wired&amp;ver=1455828505"
+
 var good = "http://www.wired.com/assets/load?scripts=true&c=1&load%5B%5D=jquery-sonar,wpcom-lazy-load-images,wired&ver=1455828505";
 
 for (var i=0; i < adScript.snapshotLength; i++) { 
     var src = adScript.snapshotItem(i).src;
     if (src == bad) {
+        src = good;
+        break;
+    }
+    if (src == ffBad) {
         src = good;
         break;
     }
